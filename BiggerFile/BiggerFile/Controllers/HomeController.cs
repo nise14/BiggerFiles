5ba63478e617f75a304fa63658d8945e5088df9a
Nicolas <nise14@htomail.com>
2023-06-02 10:20:46 -0500

    First commit

---
+++ BiggerFile/BiggerFile/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using BiggerFile.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiggerFile.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    //[RequestSizeLimit(512 * 1024 * 1024)]
+    public IActionResult Index(IFormFile archivo)
+    {
+        var pesoDelArchivoEnMegas = archivo.Length / (1024.0 * 1024);
+        return View(pesoDelArchivoEnMegas);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
